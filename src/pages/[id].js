40ab7a70ef7dfab55d3410397207cf50f5bdfad4
nt_wuruipeng <nt_wuruipeng@csmail.citicbank.com>
2021-01-04 13:49:40 +0800

    umi3 init

---
+++ src/pages/[id].js
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+export default class Id extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>id-{this.props.match.params.id}</div>
+    )
+  }
+} 
